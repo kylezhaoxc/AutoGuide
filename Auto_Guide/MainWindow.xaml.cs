923b451b4b4f14c2dde6485220cd074b820a33b3
KyleZhao <kylezhaoxc@163.com>
2015-06-05 10:23:58 +0800

    better together

--- Auto_Guide/MainWindow.xaml.cs
+++ Auto_Guide/MainWindow.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Auto_Guide
@@ -23,6 +22,13 @@ namespace Auto_Guide
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            NewRoute rt = new NewRoute(this);
+            rt.Show();
+            this.Hide();
         }
     }
 }
