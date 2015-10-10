20d7a8ad0f68a16dd7a531dcd7eaba184f5c4053
KyleZhao <kylezhaoxc@163.com>
2015-10-10 17:52:18 +0800

    Waiting to change S4M.dll to make SVMs

---
+++ TestNode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Localization;
+namespace TestNode
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Navigator nav = Navigator.GetNavigator(AppDomain.CurrentDomain.BaseDirectory+"\\nodeimages");
+            Console.Read();
+        }
+      
+    }
+}
